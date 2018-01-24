8fb684abecc7260a463f4e7f099bef345316d83b
Krishnan Mani <km@krishnanm.com>
2018-01-25 02:38:17 +0530

    Refactoring to use Date

--- lib/tags.rb
+++ lib/tags.rb
@@ -11,16 +11,20 @@ def get_date_string(now)
   now.strftime('%Y-%m-%d')
 end
 
+def get_start_and_end_dates
+  now = Date.today
+  today_str = get_date_string(now)
+  last_month_str = get_date_string((now << 1))
+  return last_month_str, today_str
+end
+
 def get_tags(credentials = nil)
   config = {region: COST_EXPLORER_API_ENDPOINT_REGION}
   config.merge!(credentials) unless credentials.nil?
 
   client = Aws::CostExplorer::Client.new(config)
 
-  now = DateTime.now
-  today_str = get_date_string(now)
-  last_month_str = get_date_string((now << 1))
-
+  last_month_str, today_str = get_start_and_end_dates
   response = client.get_tags(time_period: {
       start: last_month_str,
       end: today_str
--- spec/date_spec.rb
+++ spec/date_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "date utilities" do
 
   it 'produces a date string' do
     require 'date'
-    now = DateTime.now
+    now = Date.today
     date_str = now.strftime('%Y-%m-%d')
     expected_date_str = "#{now.year}-#{pad_two_digits(now.month)}-#{pad_two_digits(now.day)}"
     expect(date_str).to eq expected_date_str
